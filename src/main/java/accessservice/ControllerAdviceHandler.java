63dbcb4e9370fd38e5f13c584e43dcf809fb186d
Gagan <Gagan@Gagans-MacBook-Pro.local>
2016-10-23 19:52:57 +0530

    added change

--- src/main/java/accessservice/ControllerAdviceHandler.java
+++ src/main/java/accessservice/ControllerAdviceHandler.java
@@ -1,18 +1,25 @@
 package accessservice;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.HttpClientErrorException;
 
 @ControllerAdvice
+@RestController
 public class ControllerAdviceHandler {
-	@ResponseBody
-	@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "Client token is invalid")
+	// @ResponseBody
+	// @ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "Client token is
+	// invalid")
 	@ExceptionHandler(value = HttpClientErrorException.class)
-	public void httpClientExceptionHandler() {
-
+	public accessservice.ExceptionHandler httpClientExceptionHandler(HttpServletRequest request, Exception exception) {
+		accessservice.ExceptionHandler exceptionHandler = new accessservice.ExceptionHandler();
+		exceptionHandler.setStatus(HttpStatus.BAD_REQUEST.value());
+		exceptionHandler.setMessage("Client token is not correct");
+		exceptionHandler.setPath(request.getRequestURI());
+		return exceptionHandler;
 	}
 }
---
+++ src/main/java/accessservice/ExceptionHandler.java
@@ -0,0 +1,31 @@
+package accessservice;
+
+public class ExceptionHandler {
+	private int status;
+	private String message;
+	private String path;
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+}
